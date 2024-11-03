d16f564986a1e8bad01cc715d4fcd72fd0063e5f
Manish Mayank <manmayoff@gmail.com>
2024-11-03 22:20:41 +0530

    added project and github

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,8 @@ import "./Contact.css";
 const Contact = () => (
   <div className="contact-container">
     Email: manmayoff@gmail.com <br />
-    LinkedIn: https://www.linkedin.com/in/manish-mayank/
+    LinkedIn: https://www.linkedin.com/in/manish-mayank/ <br />
+    Github: https://github.com/manmayoff
   </div>
 );
 
